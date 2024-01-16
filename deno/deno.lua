a57fca5e9d222dd1000da1207b90c40c99c797b6
lihan <lihan@apache.org>
2024-01-16 16:47:22 +0800

    update time

--- deno/deno.lua
+++ deno/deno.lua
@@ -1,4 +1,4 @@
----    Copyright 2023 Han Li
+---    Copyright 2024 Han Li
 --
 --   Licensed under the Apache License, Version 2.0 (the "License");
 --   you may not use this file except in compliance with the License.
