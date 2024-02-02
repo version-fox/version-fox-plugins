13b93a59f3b6b929e390c610b1f02926b0f64d27
lihan <lihan@apache.org>
2024-02-02 21:42:18 +0800

    Update readme automatically

--- update-readme.sh
+++ update-readme.sh
@@ -2,8 +2,8 @@ readme_file="README.md"
 json_file="index.json"
 json=$(cat ${json_file})
 
-table_start="| Name | Version | Author | Description |"
-table_header="|------|---------|--------|-------------|"
+table_start="<table>\n<tr>\n<th>Name</th>\n<th>Version</th>\n<th>Author</th>\n<th>Description</th>\n</tr>"
+table_end="</table>"
 
 table_content=""
 for row in $(echo "${json}" | jq -r '.[] | @base64'); do
@@ -24,14 +24,18 @@ for row in $(echo "${json}" | jq -r '.[] | @base64'); do
       author=$(_jq_file '.plugin_author')
       desc=$(_jq_file '.plugin_desc')
 
-      table_content="${table_content}\n| ${name} | ${version} | ${author} | ${desc} |"
+      name="<small><b>${name}</b></small>"  # Make the name bold
+      version="<small>${version}</small>"  # Change the font size
+      author="<small>${author}</small>"  # Change the font size
+      desc="<small>${desc}</small>"  # Change the font size
+      desc=${desc//$'\n'/<br>}  # Replace newline characters with <br> tags
+
+      table_content="${table_content}\n<tr>\n<td>${name}</td>\n<td>${version}</td>\n<td>${author}</td>\n<td>${desc}</td>\n</tr>"
   done
 done
 echo "=======successfully generated table content========"
-echo "$table_header"
 echo "$table_content"
 # Insert the table into the README file
-sed -i "/<!-- TABLE_START -->/,/<!-- TABLE_END -->/c\\<!-- TABLE_START -->\n${table_start}\n${table_header}${table_content}\n<!-- TABLE_END -->" ${readme_file}
-
+sed -i "/<!-- TABLE_START -->/,/<!-- TABLE_END -->/c\\<!-- TABLE_START -->\n${table_start}${table_content}\n${table_end}\n<!-- TABLE_END -->" ${readme_file}
 echo "=======successfully updated README file========"
 cat ${readme_file}
