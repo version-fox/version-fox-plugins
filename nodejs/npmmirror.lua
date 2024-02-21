def6edf78b540b4703388e68a44b4913f3a7736c
野声 <artin@cat.ms>
2024-02-21 16:45:05 +0800

    Support shorthand version in Node.js (#15)

--- nodejs/npmmirror.lua
+++ nodejs/npmmirror.lua
@@ -16,7 +16,7 @@ VersionSourceUrl = "https://cdn.npmmirror.com/binaries/node/index.json"
 PLUGIN = {
     name = "nodejs",
     author = "yimiaoxiehou",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "install Node.js use https://cdn.npmmirror.com",
     updateUrl = "https://raw.githubusercontent.com/version-fox/version-fox-plugins/main/nodejs/npmmirror.lua",
 }
@@ -28,6 +28,17 @@ function PLUGIN:PreInstall(ctx)
         local lists = self:Available({})
         version = lists[1].version
     end
+
+    if not is_semver_simple(version) then
+        local lists = self:Available({})
+        local shorthands = calculate_shorthand(lists)
+        version = shorthands[version]
+    end
+
+    if (version == nil) then
+        error("version not found for provided version " .. version)
+    end
+
     local arch_type = ARCH_TYPE
     local ext = ".tar.gz"
     local osType = OS_TYPE
@@ -91,7 +102,13 @@ function get_checksum(file_content, file_name)
     return nil
 end
 
+available_result = nil
+
 function PLUGIN:Available(ctx)
+    if available_result then
+        return available_result
+    end
+
     local resp, err = http.get({
         url = VersionSourceUrl
     })
@@ -100,6 +117,7 @@ function PLUGIN:Available(ctx)
     end
     local body = json.decode(resp.body)
     local result = {}
+
     for _, v in ipairs(body) do
         table.insert(result, {
             version = string.gsub(v.version, "^v", ""),
@@ -113,11 +131,17 @@ function PLUGIN:Available(ctx)
         })
     end
     table.sort(result, compare_versions)
+    available_result = result
     return result
 end
 
 --- Expansion point
 function PLUGIN:PostInstall(ctx)
+    local rootPath = ctx.rootPath
+    local sdkInfo = ctx.sdkInfo['nodejs']
+    local path = sdkInfo.path
+    local version = sdkInfo.version
+    local name = sdkInfo.name
 end
 
 function PLUGIN:EnvKeys(ctx)
@@ -137,4 +161,47 @@ function PLUGIN:EnvKeys(ctx)
             },
         }
     end
+end
+
+function is_semver_simple(str)
+    -- match pattern: three digits, separated by dot
+    local pattern = "^%d+%.%d+%.%d+$"
+    return str:match(pattern) ~= nil
+end
+
+function extract_semver(semver)
+    local pattern = "^(%d+)%.(%d+)%.[%d.]+$"
+    local major, minor = semver:match(pattern)
+    return major, minor
+end
+
+function calculate_shorthand(list)
+    local versions_shorthand = {}
+    for _, v in ipairs(list) do
+        local version = v.version
+        local major, minor = extract_semver(version)
+
+        if major then
+            if not versions_shorthand[major] then
+                versions_shorthand[major] = version
+            else
+                if compare_versions({version = version}, {version = versions_shorthand[major]}) then
+                    versions_shorthand[major] = version
+                end
+            end
+
+            if minor then
+                local major_minor = major .. "." .. minor
+                if not versions_shorthand[major_minor] then
+                    versions_shorthand[major_minor] = version
+                else
+                    if compare_versions({version = version}, {version = versions_shorthand[major_minor]}) then
+                        versions_shorthand[major_minor] = version
+                    end
+                end
+            end
+        end
+    end
+
+    return versions_shorthand
 end
