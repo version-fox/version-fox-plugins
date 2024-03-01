e43cb7dda14785f4fc285a2671307ae382728c34
ahai <1149956305@qq.com>
2024-03-01 15:32:39 +0800

    gradle

---
+++ gradle/gradle.lua
@@ -0,0 +1,98 @@
+
+--- Common libraries provided by VersionFox (optional)
+local http = require("http")
+
+--- The following two parameters are injected by VersionFox at runtime
+--- Operating system type at runtime (Windows, Linux, Darwin)
+OS_TYPE = ""
+--- Operating system architecture at runtime (amd64, arm64, etc.)
+ARCH_TYPE = ""
+
+PLUGIN = {
+    --- Plugin name
+    name = "gradle",
+    --- Plugin author
+    author = "ahai",
+    --- Plugin version
+    version = "0.0.1",
+    --- Plugin description
+    description = "gradle",
+    -- Update URL
+    updateUrl = "https://raw.githubusercontent.com/version-fox/version-fox-plugins/main/gradle/gradle.lua",
+    -- minimum compatible vfox version
+    minRuntimeVersion = "0.2.3",
+}
+
+AvailableVersionsUrl = "https://gradle.org/releases/"
+DownloadInfoUrl = "https://services.gradle.org/distributions/gradle-%s-bin.zip"
+
+
+function PLUGIN:PreInstall(ctx)
+    local version = ctx.version
+    local downloadUrl = DownloadInfoUrl:format(version)
+
+    local resp, err = http.get({
+        url = downloadUrl..".sha256"
+    })
+    if err ~= nil then
+        error(err)
+    end
+
+    if resp.status_code ~= 200 then
+        return nil
+    end
+
+    local sha256 = resp.body
+
+    return {
+        version = version,
+        sha256 = sha256,
+        url = downloadUrl,
+    }
+end
+
+function PLUGIN:PostInstall(ctx)
+   
+end
+
+
+
+function PLUGIN:Available(ctx)
+    --- get htmlBody
+    local resp, err = http.get({
+        url = AvailableVersionsUrl
+    })
+    if err ~= nil or resp.status_code ~= 200 then
+        return {}
+    end
+    local htmlBody = resp.body
+    --- get version
+    local htmlContent= [[]] .. htmlBody .. [[]]
+
+    local result = {}
+
+    for version in htmlContent:gmatch('<a name="(.-)"></a>') do
+        table.insert(result, {version=version,note=""})
+    end
+
+    return result
+end
+
+
+function PLUGIN:EnvKeys(ctx)
+    local path = ctx.path
+    if OS_TYPE == "darwin" then
+        path = path .. "/Contents/Home"
+    end
+
+    return {
+        {
+            key = "GRADLE_HOME",
+            value = path
+        },
+        {
+            key = "PATH",
+            value = path .. "/bin"
+        }
+    }
+end
